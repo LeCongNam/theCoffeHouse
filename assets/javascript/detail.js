1fbd355417b264f0b8283262dad3774f9c7fe1d2
Lê Công Nam <namlem4u@gmail.com>
2022-01-05 10:37:16 +0700

    update mobile

--- assets/javascript/detail.js
+++ assets/javascript/detail.js
@@ -1,16 +1,18 @@
-var listSize = document.getElementById('list-size-price')// thẻ ul
-var liItem = listSize.querySelectorAll('li'); // thẻ li
+(function chooseSize(params) {
+    var listSize = document.getElementById('list-size-price')// thẻ ul
+    var liItem = listSize.querySelectorAll('li'); // thẻ li
 
-sizeChoose = Array.from(liItem)
+    sizeChoose = Array.from(liItem)
 
-for (const aElement of sizeChoose) {
-    aElement.onclick = function (e) {
-        e.preventDefault();
-        let li = e.target.closest('li')
-        // remove active default
-        let liActive = listSize.querySelector('li.active')
-        liActive.classList.remove('active')
-        // add active
-        li.classList.add('active')
+    for (const aElement of sizeChoose) {
+        aElement.onclick = function (e) {
+            e.preventDefault();
+            let li = e.target.closest('li')
+            // remove active default
+            let liActive = listSize.querySelector('li.active')
+            liActive.classList.remove('active')
+            // add active
+            li.classList.add('active')
+        }
     }
-}
+})()
